915fbf304237a721d656d8510f16f667e99895de
Sergey <116816800+rol1987@users.noreply.github.com>
2024-05-04 09:08:35 +0300

    первый коммит

---
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=128, verbose_name='Тэг')
+
+    # publish = models.ManyToManyField(Article, through='Scope')
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=256, verbose_name='Название')
+    text = models.TextField(verbose_name='Текст')
+    published_at = models.DateTimeField(verbose_name='Дата публикации')
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение')
+    scope = models.ManyToManyField(Tag, through='Scope')
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
+
+    def __str__(self):
+        return self.title
+
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    is_main = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.article.title + " " + self.tag.name
+
+    class Meta:
+        verbose_name = 'Связь'
+        verbose_name_plural = 'Связи'
+        ordering = ['-is_main', 'tag__name']
